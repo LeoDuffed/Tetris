1da447391544a3e12bb4c367593dab9ec5053fb5
LeoDuffed <carlosmroder@gmail.com>
2025-02-04 11:22:02 -0600

    Sigo sin lograr que funcione

--- main.cpp
+++ main.cpp
@@ -1,7 +1,6 @@
 #include <iostream> 
 #include <SFML/Graphics.hpp>
-#include <SFML/Window.hpp>
-#include <variant>
+#include <SFML/Window/Event.hpp>
 using namespace std;
 using namespace sf;
 
@@ -9,17 +8,16 @@ int main(){
     RenderWindow window(VideoMode({400,400}),"Tetris");
     window.setFramerateLimit(60);
     while(window.isOpen()){
-        std::optional<Event> event;
-        while((event =window.pollEvent())){
-            if(std::holds_alternative<sf::Event::ClosedEvent>(*event)){
-            window.close();
+        Event event;
+        while(window.pollEvent()){
+            cout << "Event: " << event.type << endl;
 
             }
 
         }
         window.clear(Color(20,20,20));
         window.display();
-    }
+    
     
     return 0;
 
