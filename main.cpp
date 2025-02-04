8a04710932c562e066fed97b3466a449779d0d01
LeoDuffed <carlosmroder@gmail.com>
2025-02-03 19:03:15 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,5 +5,18 @@ using namespace std;
 using namespace sf;
 
 int main(){
+    RenderWindow window(VideoMode(400,400),"Tetris");
+    window.setFramerateLimit(60);
+    while(window.isOpen()){
+        Event event;
+        while(window.pollEvent(event)){
+            if(event.type==Event::Closed) window.close();
+
+        }
+        window.clear(Color(20,20,20));
+        window.display();
+    }
+    
+    return 0;
 
 }
