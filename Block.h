6baf0527e7a205518ef62b9100675d4fac4deb9c
LeoDuffed <carlosmroder@gmail.com>
2025-02-07 13:27:20 -0600

    Crando clase padre para los cloques

---
+++ Block.h
@@ -0,0 +1,19 @@
+#ifndef Block_H
+#define Block_H
+#include <iostream>
+using namespace std;
+
+class Block{
+    private:
+
+    public:
+    Block();
+    ~Block(){}
+
+    int id;
+    
+
+
+};
+
+#endif
---
+++ Position.h
@@ -0,0 +1,15 @@
+#ifndef Position_H
+#define Position_H
+#include <iostream> 
+#pragma once
+using namespace std;
+
+class Position{
+    public:
+    Position(int row, int column);
+    int row;
+    int column;
+
+};
+
+#endif
