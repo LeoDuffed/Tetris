679a5a6f4b87b72b709c3bf3b8ed2e327dde7369
LeoDuffed <carlosmroder@gmail.com>
2025-02-06 23:24:33 -0600

    Grid.h termiando

--- Grid.h
+++ Grid.h
@@ -5,8 +5,26 @@ using namespace std;
 
 class Grid{
     private:
+    
+    int numRows;
+    int numCols;
+    int cellSize;
+
 
     public:
+
+    Grid();
+    ~Grid(){}
+    Grid(int numRows, int numCols, int cellSize);
+    void setNumRows(int numRows);
+    void setNumCols(int numCols);
+    void setCellSize(int cellSize);
+    int getNumRows();
+    int getNumCols();
+    int getCellSize();
+
+    int grid[20][10];
+
 
 };
 
