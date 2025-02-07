7ee5c107b5ca5740f08d7455775dfa07f194eb93
LeoDuffed <carlosmroder@gmail.com>
2025-02-06 22:23:05 -0600

    ya esta funcionando

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <raylib.h>
+
+int main(){
+
+    InitWindow(300,600, "raylib Tetris");
+
+    while(WindowShouldClose()==false){
+
+        BeginDrawing();
+
+        EndDrawing();
+    }
+
+    CloseWindow();
+}
