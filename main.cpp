c1dd5374e506e8b984c23a8caea4b1909347d4cc
LeoDuffed <carlosmroder@gmail.com>
2025-02-12 20:06:52 -0600

    Cambios en la interfaz

--- main.cpp
+++ main.cpp
@@ -34,8 +34,22 @@ int main(){
 
         BeginDrawing();
         ClearBackground(darkBlue);
-        DrawTextEx(font, "Score", {355, 65}, 38, 2, WHITE);
-        DrawRectangleRounded({325,110,170,60},0.3,6,lightBlue);
+
+        DrawTextEx(font, "T", {323, 40}, 38, 2, RED);
+        DrawTextEx(font, "E", {353, 40}, 38, 2, ORANGE);
+        DrawTextEx(font, "T", {380, 40}, 38, 2, YELLOW);
+        DrawTextEx(font, "R", {410, 40}, 38, 2, GREEN);
+        DrawTextEx(font, "I", {440, 40}, 38, 2, cyan);
+        DrawTextEx(font, "S", {455, 40}, 38, 2, PURPLE);
+
+        DrawTextEx(font, "Score", {350, 165}, 38, 2, WHITE);
+        if(game.gameOver){
+            DrawTextEx(font, "GAME", {350, 430}, 40, 2, WHITE);
+            DrawTextEx(font, "OVER", {350, 470}, 40, 2, WHITE);
+
+
+        }
+        DrawRectangleRounded({320,210,170,60},0.3,6,lightBlue);
         game.Draw();
         EndDrawing();
     }
