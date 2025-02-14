92ef7cea46fa0ac73e40e21f2f0a16580f5ee1f0
LeoDuffed <carlosmroder@gmail.com>
2025-02-14 09:02:28 -0600

    Cambios en la interfaz

--- main.cpp
+++ main.cpp
@@ -45,8 +45,6 @@ int main(){
         DrawTextEx(font, "I", {440, 40}, 38, 2, cyan);
         DrawTextEx(font, "S", {455, 40}, 38, 2, PURPLE);
 
-        DrawTextEx(font, "Score", {350, 165}, 38, 2, WHITE);
-
         if(game.gameOver){
             DrawRectangleRounded({315,480,182,100},0.3,6,lightBlue);
             DrawTextEx(font, "GAME", {350, 495}, 40, 2, WHITE);
@@ -59,24 +57,25 @@ int main(){
             DrawTextEx(font, "En Pausa", {320, 520}, 40, 2, WHITE);
         }
 
-        DrawRectangleRounded({320,210,170,60},0.3,6,lightBlue);
+        DrawTextEx(font, "Score", {350, 130}, 38, 2, WHITE);
+
+        DrawRectangleRounded({320,175,170,60},0.3,6,lightBlue);
         char scoreText[10];
         snprintf(scoreText, sizeof(scoreText), "%d", game.score); 
         Vector2 textSize = MeasureTextEx(font, scoreText, 38, 2); 
 
         float textPosX = 300 + (170 - textSize.x) / 2;
 
-        DrawTextEx(font, scoreText, {textPosX, 225}, 38, 2, WHITE);
+        DrawTextEx(font, scoreText, {textPosX, 190}, 38, 2, WHITE);
+
+        DrawTextEx(font, "Top",{380, 300},28,2, WHITE);
 
         vector<pair<int, string>> scores = game.getHighScores();
         for (int i = 0; i < scores.size(); i++) {
             char scoreEntry[50];
             snprintf(scoreEntry, sizeof(scoreEntry), "%d. %s: %d", i + 1, scores[i].second.c_str(), scores[i].first);
             DrawTextEx(font, scoreEntry, (Vector2){340.0f, static_cast<float>(350 + i * 30)}, 24, 2, WHITE);
-        }
-
-        DrawTextEx(font, "Top",{380, 300},28,2, WHITE);
-         
+        }         
 
         game.Draw();
         EndDrawing();
